2c5ea72002fddd27efc1a408dbe8f66afc3a9244
Hariaravind4 <165349652+Hariaravind4@users.noreply.github.com>
2024-03-29 08:15:30 +0530

    Add files via upload

---
+++ PART-A_1.py
@@ -0,0 +1,5 @@
+
+n=6     #no of faces in a dice
+totalCombinations=n*n
+print("The total possible combinations is",totalCombinations)
+#combinations is 6^2 because there are two dices with each dice having 6 faces each thus 6*6
